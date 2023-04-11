6838f47bd30139ab0c76bb521a3e29642ee1581d
JJCA <75126343+J-Snowwite@users.noreply.github.com>
2023-04-11 10:06:40 +0800

    Update getMd5.sh

--- getMd5.sh
+++ getMd5.sh
@@ -3,6 +3,6 @@
 
 for image_name in `cat nameList`
 do
-	md5=$(curl -u xujintai:Admin@123 http://resource.elextec.com/artifactory/api/storage/ELEX-Generic-Dev-Local/EBase/FG-resource-off/unPassImage/debian11_base_en.raw|jq '.checksums.md5'|sed 's/\"//g');
+	md5=$(curl -u AAA:AAA http://resource.XXX.com/artifactory/api/storage/xxx-Generic-Dev-Local/EBase/FG-resource-off/unPassImage/debian11_base_en.raw|jq '.checksums.md5'|sed 's/\"//g');
 	echo $image_name $md5;
 done
