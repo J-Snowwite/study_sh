acaffed43656f27364512c248487032a1f77fd66
JJCA <75126343+J-Snowwite@users.noreply.github.com>
2023-04-11 10:05:55 +0800

    Update getDirImages.sh

--- getDirImages.sh
+++ getDirImages.sh
@@ -1,16 +1,16 @@
 #/bin/bash
 set -x
 
-Repository_Path="ELEX-Generic-Dev-Local/EBase/FG-resource-off/unPassImage/"
-url="https://resource.elextec.com/artifactory/"
+Repository_Path="XXX-Generic-Dev-Local/EBase/FG-resource-off/unPassImage/"
+url="https://resource.XXXX.com/artifactory/"
 apiurl="api/storage/"
 
-num1=$(curl -u xujintai:Admin@123 $url$apiurl$Repository_Path |jq '.children'|grep uri|wc -l)
+num1=$(curl -u AAA:AAA $url$apiurl$Repository_Path |jq '.children'|grep uri|wc -l)
 echo "共 $num1 个镜像需要下载"
 
-for filename in $(curl -u xujintai:Admin@123 $url$apiurl$Repository_Path |jq '.children'|grep uri|awk '{ print $2}'|sed "s/\"//g"|sed "s/\///g"|sed "s/\,//g" ); 
+for filename in $(curl -u AAA:AAA $url$apiurl$Repository_Path |jq '.children'|grep uri|awk '{ print $2}'|sed "s/\"//g"|sed "s/\///g"|sed "s/\,//g" ); 
 do
 	echo "下载第$num1个镜像："$filename;
-	wget --user xujintai --password Admin@123 $url$Repository_Path$filename;
+	wget --user AAA --password AAA $url$Repository_Path$filename;
 done
 
